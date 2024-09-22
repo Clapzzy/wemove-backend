6202c19336958a659c79bb4c4bfc0b01df4c2b61
Clapzzy <martin.kurtew@gmail.com>
2024-09-22 19:11:24 +0300

    wont

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const app = express();
 const port = 3000;
 const jwt = require('jsonwebtoken');
 
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
+
 //All routes
 const user = require('./rourte/user');
 const auth = require('./rourte/auth')
